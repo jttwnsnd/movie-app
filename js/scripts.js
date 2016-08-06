d33de4be0053ec25baebb6d272d235010d6c62a6
jttwnsnd <jt.a.townsend@gmail.com>
2016-08-06 09:57:08 -0400

    Added bxSlider pluggin

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,36 @@ $(document).ready(function(){
 	var imgBaseUrl = 'http://image.tmdb.org/t/p/'
 	// the query string, including the api key
 	var apiKey = 'api_key=fec8b5ab27b292a68294261bb21b04a5';
-	var userSearchOptions = 'now_playing'
+	var userPopular = 'popular';
+	var userNowPlaying = 'now_playing';
+	var userSearchOptions = 'popular';
+	var userUpcoming = 'upcoming';
+
 	var npUrl = apiBaseUrl + 'movie/'+ userSearchOptions + '?' + apiKey;
 	var genreListUrl = apiBaseUrl + 'genre/movie/list?' + apiKey;
+	//get images and place them inside slider
+	function slideShowImages(imageGroup, location){
+		var imgGroupUrl = apiBaseUrl + 'movie/'+ imageGroup + '?' + apiKey;
+		$.getJSON(imgGroupUrl,function(imageData){
+			var bxSliderHTML = '';
+			for(var i = 0; i < 5; i++){
+				var posterUrl = imgBaseUrl + 'w300'+ imageData.results[i].poster_path;
+				bxSliderHTML += '<li><img src ="' + posterUrl + '" title="'+ imageData.results[i].name +'"></li>';
+			}
+			$(location).html(bxSliderHTML);
+			$(location).bxSlider({
+				slideWidth: 150
+			});
+			console.log(bxSliderHTML);
+		})
+	}
+	slideShowImages(userNowPlaying, '.bxslider-np');
+	slideShowImages(userPopular, '.bxslider-pop');
+	slideShowImages(userUpcoming, '.bxslider-upcoming');
 	//create a function for calling the JSON.
 	function callJson(npUrl){
 		$.getJSON(npUrl, function(theData){
 			$('.poster_grid').html('');
-			console.log(theData);
 			var npHTML = '';
 			for(var i = 0; i < theData.results.length; i++){
 				npHTML += '<figure class="col">';
@@ -31,12 +53,25 @@ $(document).ready(function(){
 			})
 		})
 	}
-	callJson(npUrl);
+	// callJson(npUrl);
 	$('.input-form').submit(function(){
 		event.preventDefault();
 		$('.poster-grid').html('');
 		var searching = $('.search').val();
 		var searchUrl = apiBaseUrl + 'search/movie?query='+ searching + '&' + apiKey;
 		callJson(searchUrl);
+	})
+	$('.selector').hover(function(){
+		if($(this).html() === 'Now Playing'){
+			var selectorId = '.np';
+			$(selectorId).css({'opacity':'1'});
+		}
+		// }else if($(this).html === "Popular"){
+		// 	var selectorType = '-pop';
+		// }else if($(this).html === "Upcoming"){
+		// 	var selectorType = '-upcoming';
+		// }
+		// var newSelector = '.bxslider' + selectorType;
+		// $(newSelector).css({'opacity':'1'});
 	})
 })
